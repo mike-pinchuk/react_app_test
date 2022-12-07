1a926fa2cc9b56eb5f9df736102c25eab34d48ac
mike <mike@MacBook-Air-mike.local>
2022-12-07 22:30:08 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { default as ReactSelect } from "react-select";
+import "./index.css";
+import Option from "./Option";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      optionSelected: null,
+      data: null,
+    };
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+  filterData() {
+    axios.post("http://localhost:8000/api", {
+      data: this.state.optionSelected,
+    });
+  }
+
+  handleChange = (selected) => {
+    this.setState({
+      optionSelected: selected,
+    });
+    this.filterData()
+  };
+
+  componentDidMount() {
+    const arrayData = [];
+    axios
+      .get("http://localhost:8000/api")
+      .then((res) => {
+        res.data.data.forEach((el) =>
+          arrayData.push({ value: el.task, label: el.task })
+        );
+        this.setState({ data: arrayData });
+        this.handleChange(JSON.parse(res.data.filteredData).data);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  componentDidUpdate() {
+    this.filterData();
+  }
+
+  render() {
+    return (
+      <span
+        className="d-inline-block"
+        data-toggle="popover"
+        data-trigger="focus"
+        data-content="Please selecet account(s)"
+      >
+        <ReactSelect
+          options={this.state.data}
+          isMulti
+          closeMenuOnSelect={false}
+          hideSelectedOptions={false}
+          components={{
+            Option,
+          }}
+          onChange={this.handleChange}
+          allowSelectAll={true}
+          value={this.state.optionSelected}
+        />
+      </span>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
